43d0fd2c279134ee9db2a0e61a809789de25ba18
wb <747184472@qq.com>
2017-09-23 16:09:48 +0800

    小修改

--- src/com/algorithm/examination/combination/Combination.java
+++ src/com/algorithm/examination/combination/Combination.java
@@ -17,7 +17,7 @@ public class Combination {
 		long[][] dp = new long[N + 1][N + 1];
 		long[] results = new long[N + 1];
 		for (int i = 1; i < N + 1; i++) {
-			for (int j = 1; j < N + 1; j++) {
+			for (int j = i; j < N + 1; j++) {
 				if (i == j)
 					dp[i][j] = factorial(i);
 				else if (i == 1)
